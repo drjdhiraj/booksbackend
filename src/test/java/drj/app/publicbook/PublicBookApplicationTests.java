9c852b6dcf5adc7e87ef35c73f2cf29840c95e51
Dhiraj Raj <dhirajraj7227@gmail.com>
2024-06-20 21:56:06 +0530

    sdf

---
+++ src/test/java/drj/app/publicbook/PublicBookApplicationTests.java
@@ -0,0 +1,13 @@
+package drj.app.publicbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
